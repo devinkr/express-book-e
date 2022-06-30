5e3d9f14f6cee5ae35dc33a7eff219b48e04e6e7
Devin Raleigh <devinkr@gmail.com>
2022-06-30 14:01:53 -0600

    Create route for bookmarks/id

--- controllers/bookmarksController.js
+++ controllers/bookmarksController.js
@@ -18,5 +18,35 @@ router.get('/', (req, res, next) => {
 		.catch(next);
 });
 
+// Using async/await
+// Index: GET all the bookmarks
+/* router.get('/', async (req, res, next) => {
+	try {
+		// 1. Get all of the bookmarks from the DB
+		const bookmarks = await Bookmark.find({});
+		// 2. Send them back to the client as JSON
+		res.json(bookmarks);
+	} catch (err) {
+		// if there's an error, pass it on!
+		next(err);
+	}
+}); */
+
+// localhost:8000/api/bookmarks/:id
+// GET: Show route
+router.get('/:id', async (req, res, next) => {
+	try {
+		Bookmark.findById(req.params.id, (err, bookmark) => {
+			if (bookmark) {
+				return res.json(bookmark);
+			} else {
+				return res.sendStatus(404);
+			}
+		});
+	} catch (err) {
+		next(err);
+	}
+});
+
 // Export this router object so that it is accessible when we require the file elsewhere
 module.exports = router;
